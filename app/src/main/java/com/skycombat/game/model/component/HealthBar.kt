c93736dfb37279f14ae8769ae7fbc74a94b66e95
Alberto Sinigaglia <sinigagliaalberto@gmail.com>
2021-03-06 16:27:27 +0100

    ultime fix

--- app/src/main/java/com/skycombat/game/model/component/HealthBar.kt
+++ app/src/main/java/com/skycombat/game/model/component/HealthBar.kt
@@ -2,6 +2,8 @@ package com.skycombat.game.model.component
 
 import android.graphics.*
 import com.skycombat.game.model.HasHealth
+import com.skycombat.game.model.support.Drawable
+import com.skycombat.game.model.support.Updatable
 
 /**
  * Represents the healthbar
@@ -9,19 +11,19 @@ import com.skycombat.game.model.HasHealth
  * @param paint : used to draw onto Canvas the healthbar
  * @param element : to determine if the element has health remaining
  */
-class HealthBar(private val initialLife: RectF, var paint: Paint, var element : HasHealth) {
+class HealthBar(private val initialLife: RectF, var paint: Paint, var element : HasHealth) : GUIComponent{
     private var life = RectF(initialLife);
     /**
      * Draws the healthbar
      * @param canvas : the canvas onto which the enemy will be drawn
      */
-    fun draw(canvas: Canvas?) {
+    override fun draw(canvas: Canvas?) {
         canvas?.drawRect(life, paint)
     }
     /**
      * Updates the current and Maxhealth of these elements
      */
-    fun update() {
+    override fun update() {
         val curHealth = element.getCurrentHealth()
         val maxHealth = curHealth.coerceAtLeast(element.getMaxHealth())
         life.right = initialLife.left + (initialLife.right - initialLife.left) * (curHealth / maxHealth)
