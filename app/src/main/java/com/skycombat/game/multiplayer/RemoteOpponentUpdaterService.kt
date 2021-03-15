c5baf1999f042c2dd1e9ef267a32d989a29c4d12
Alberto Sinigaglia <sinigagliaalberto@gmail.com>
2021-03-15 17:16:35 +0100

    test multi 3

--- app/src/main/java/com/skycombat/game/multiplayer/RemoteOpponentUpdaterService.kt
+++ app/src/main/java/com/skycombat/game/multiplayer/RemoteOpponentUpdaterService.kt
@@ -1,11 +1,14 @@
 package com.skycombat.game.multiplayer
 
 import android.util.Log
+import com.amplifyframework.api.graphql.GraphQLResponse
+import com.amplifyframework.api.graphql.PaginatedResult
 import com.amplifyframework.api.graphql.model.ModelQuery
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.core.model.temporal.Temporal
 import com.amplifyframework.datastore.generated.model.Player
 import com.skycombat.game.model.gui.element.ghost.Ghost
+import kotlinx.coroutines.internal.synchronized
 import java.util.concurrent.TimeUnit
 
 class RemoteOpponentUpdaterService(var currentPlayer: Player, private var opponents : List<Pair<Player, Ghost>>) : OpponentsUpdater(){
@@ -22,54 +25,57 @@ class RemoteOpponentUpdaterService(var currentPlayer: Player, private var oppone
         super.run()
         while(alive){
             Amplify.API.query(
-                ModelQuery.list(
-                    Player::class.java,
-                    Player.GAMEROOM.eq(currentPlayer.gameroom.id)
-                        .and(Player.LASTINTERACTION.gt(
-                            Temporal.Timestamp(
-                                System.currentTimeMillis() - 10000L,
-                                TimeUnit.MILLISECONDS
-                            ))
-                        )
-                        .and(Player.DEAD.eq(false))
-                        .and(Player.ID.ne(currentPlayer.id))
-                ),
-                { result ->
-                    if(alive) {
-                        if (result.data.items.count() <= 0) {
-                            opponents.forEach { el ->
-                                el.second.kill()
-                            }
-                            Log.e("FINE", "TUTTI MORTI, SPENGO THREAD OPPONENTI")
-                            this.stopUpdates()
-                        }
-                        val present: (Pair<Player, Ghost>) -> Boolean = { el ->
-                            result.data.items.any { res -> res.id == el.first.id }
-                        }
-                        val partitions = opponents
-                                .partition { el -> present(el) && !el.second.isDead() }
+                    ModelQuery.list(
+                            Player::class.java,
+                            Player.GAMEROOM.eq(currentPlayer.gameroom.id)
+                                    .and(Player.LASTINTERACTION.gt(
+                                            Temporal.Timestamp(
+                                                    System.currentTimeMillis() - 10000L,
+                                                    TimeUnit.MILLISECONDS
+                                            ))
+                                    )
+                                    .and(Player.DEAD.eq(false))
+                                    .and(Player.ID.ne(currentPlayer.id))
+                    ),
+                    { result ->
+                        updateOpponents(result)
 
-                        // quelli presenti nella risposta dell'API
-                        partitions.first.forEach { p ->
-                            val op = result.data.items.first { req -> req.id == p.first.id }
-                            p.second.aimedPositionX =
-                                    op.positionX.toFloat() * p.second.context.width
-                        }
-
-                        // quelli non presenti nella risposta dell'API
-                        partitions.second.forEach { p ->
-                            Log.e("idk", "È MORTO ${p.first.name}")
-                            p.second.kill()
-                        }
-                    }
-
-                },
-                { Log.e("MyAmplifyApp", "Query failed") }
+                    },
+                    { Log.e("MyAmplifyApp", "Query failed") }
             )
             sleep(1000L/MultiplayerSession.UPS)
         }
     }
-    override fun stopUpdates(){
+    @Synchronized fun updateOpponents(result: GraphQLResponse<PaginatedResult<Player>>) {
+        if (alive) {
+            if (result.data.items.count() <= 0) {
+                opponents.forEach { el ->
+                    el.second.kill()
+                }
+                Log.e("FINE", "TUTTI MORTI, SPENGO THREAD OPPONENTI")
+                this.stopUpdates()
+            }
+            val present: (Pair<Player, Ghost>) -> Boolean = { el ->
+                result.data.items.any { res -> res.id == el.first.id }
+            }
+            val partitions = opponents
+                    .partition { el -> present(el) && !el.second.isDead() }
+
+            // quelli presenti nella risposta dell'API
+            partitions.first.forEach { p ->
+                val op = result.data.items.first { req -> req.id == p.first.id }
+                p.second.aimedPositionX =
+                        op.positionX.toFloat() * p.second.context.width
+            }
+
+            // quelli non presenti nella risposta dell'API
+            partitions.second.forEach { p ->
+                Log.e("idk", "È MORTO ${p.first.name}")
+                p.second.kill()
+            }
+        }
+    }
+    @Synchronized override fun stopUpdates(){
         if(alive) {
             Log.e("test", "INIZIO STOPPO CICLO")
             this.alive = false;
