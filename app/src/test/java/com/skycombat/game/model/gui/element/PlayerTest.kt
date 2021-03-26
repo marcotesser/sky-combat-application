10e1f683104cb43e3bd05d3deced6ea5b614f5b2
ApocalypseTH <36929115+ApocalypseTH@users.noreply.github.com>
2021-03-26 17:06:07 +0100

    Update PlayerTest.kt

--- app/src/test/java/com/skycombat/game/model/gui/element/PlayerTest.kt
+++ app/src/test/java/com/skycombat/game/model/gui/element/PlayerTest.kt
@@ -2,7 +2,17 @@ package com.skycombat.game.model.gui.element
 
 import android.graphics.PointF
 import android.view.animation.LinearInterpolator
+import com.skycombat.game.model.factory.bullet.BulletFactory
+import com.skycombat.game.model.factory.bullet.LaserBulletFactory
+import com.skycombat.game.model.geometry.Rectangle
 import com.skycombat.game.model.gui.DisplayDimension
+import com.skycombat.game.model.gui.element.bullet.Bullet
+import com.skycombat.game.model.gui.element.bullet.ClassicBullet
+import com.skycombat.game.model.gui.element.bullet.LaserBullet
+import com.skycombat.game.model.gui.element.bullet.collision.EnemyCollisionStrategy
+import com.skycombat.game.model.gui.element.enemy.Enemy
+import com.skycombat.game.model.gui.element.enemy.JetEnemy
+import com.skycombat.game.model.gui.element.enemy.movement.Movement
 import com.skycombat.game.model.gui.element.ghost.movement.LinearAimedPositionMovement
 import org.junit.Assert.*
 import org.junit.Test
@@ -18,6 +28,14 @@ class PlayerTest {
     }
 
     @Test
+    fun `shouldn't you be alive?`(){
+        var player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(10f, 10f))
+        player.health = 1f
+
+        assertTrue(player.isAlive())
+    }
+
+    @Test
     fun `center is valid`(){
         val width = 100f; val height = 100f
         var player = Player(0f, LinearAimedPositionMovement(), DisplayDimension(width, height))
@@ -30,4 +48,47 @@ class PlayerTest {
 
         assertTrue(tx==x && ty==y)
     }
+
+    @Test
+    fun `collide with bullet`(){
+        val width = 100f; val height = 100f
+//        var enemy = JetEnemy(LaserBulletFactory(), Movement(1,2,3), DisplayDimension(width, height))
+        var player = Player(0f, LinearAimedPositionMovement(), DisplayDimension(width, height))
+        var bullet = ClassicBullet(1f,1f, EnemyCollisionStrategy(),  Bullet.Direction.DOWN, DisplayDimension(width, height))
+
+        player.setPosition(1f, 1f)
+
+        assertTrue(player.collide(bullet))
+    }
+
+    @Test
+    fun `check health if bullet collide`(){
+        val width = 100f; val height = 100f
+//        var enemy = JetEnemy(LaserBulletFactory(), Movement(1,2,3), DisplayDimension(width, height))
+        var player = Player(0f, LinearAimedPositionMovement(), DisplayDimension(width, height))
+        var bullet = ClassicBullet(1f,1f, EnemyCollisionStrategy(),  Bullet.Direction.DOWN, DisplayDimension(width, height))
+
+        player.setPosition(1f, 1f)
+        bullet.applyCollisionEffects(player)
+
+        assertTrue(player.getCurrentHealth()<player.getMaxHealth())
+    }
+
+    @Test
+    fun `check MAX_HEALTH with shield if bullet collide`(){
+        val width = 100f; val height = 100f
+        var player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(10f, 10f))
+        var bullet = ClassicBullet(1f,1f, EnemyCollisionStrategy(),  Bullet.Direction.DOWN, DisplayDimension(width, height))
+
+        player.applyShield(2)
+
+        player.setPosition(1f, 1f)
+        bullet.applyCollisionEffects(player)
+
+        assertTrue(player.getCurrentHealth() == player.getMaxHealth())
+    }
+
+
+
 }
+
