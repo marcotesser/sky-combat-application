b48c134148607b76b886bbd71688562ffe263d94
Alice Zago <alice.zago.3@studenti.unipd.it>
2021-03-27 16:45:12 +0100

    Update EnemyTest.kt

--- app/src/test/java/com/skycombat/game/model/gui/element/enemy/EnemyTest.kt
+++ app/src/test/java/com/skycombat/game/model/gui/element/enemy/EnemyTest.kt
@@ -2,12 +2,9 @@ package com.skycombat.game.model.gui.element.enemy
 
 import com.skycombat.game.model.factory.bullet.GustBulletFactory
 import com.skycombat.game.model.factory.bullet.LaserBulletFactory
-import com.skycombat.game.model.factory.bullet.MultipleBulletFactory
 import com.skycombat.game.model.gui.DisplayDimension
-import com.skycombat.game.model.gui.element.bullet.Bullet
-import com.skycombat.game.model.gui.element.bullet.collision.PlayerCollisionStrategy
 import com.skycombat.game.model.gui.element.enemy.movement.Movement
-import com.skycombat.game.model.gui.element.ghost.movement.LinearAimedPositionMovement
+
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -25,7 +22,8 @@ class EnemyTest {
 
     @Test
     fun `getMaxHealth plane enemy`() {
-        val width = 100f; val height = 100f
+        val width = 100f
+        val height = 100f
         val coeff = 6f
         val enemy = PlaneEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height), coeff)
         val health = 200f * coeff
@@ -34,7 +32,8 @@ class EnemyTest {
 
     @Test
     fun `getMaxHealth jet enemy`() {
-        val width = 100f; val height = 100f
+        val width = 100f
+        val height = 100f
         val enemy = JetEnemy(LaserBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
         val health = 300f
         assertEquals(health , enemy.getMaxHealth())
@@ -42,7 +41,8 @@ class EnemyTest {
 
     @Test
     fun shouldRemove() {
-        val width = 100f; val height = 100f
+        val width = 100f
+        val height = 100f
         val enemy = SpaceShipEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
         enemy.health = 0f
 
