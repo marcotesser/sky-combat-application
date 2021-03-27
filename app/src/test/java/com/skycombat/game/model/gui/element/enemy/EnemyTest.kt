f622868c21cf22124d6a3598a9bcdd1d698bf7f4
Alice Zago <alice.zago.3@studenti.unipd.it>
2021-03-27 16:18:52 +0100

    Update EnemyTest.kt

--- app/src/test/java/com/skycombat/game/model/gui/element/enemy/EnemyTest.kt
+++ app/src/test/java/com/skycombat/game/model/gui/element/enemy/EnemyTest.kt
@@ -15,52 +15,38 @@ import org.junit.Assert.*
 class EnemyTest {
 
     @Test
-    fun update() {
-        //val width = 100f; val height = 100f
-        //var enemy =PlaneEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
-        //while (!enemy.shouldRemove())
-            //enemy.update()
-        //assert()
+    fun getHeight() {
+        val width = 100f; val height = 100f
+        val enemy = PlaneEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
+        val high = 180f
+        val top = enemy.getHeight()
+        assertEquals(high,top)
     }
 
     @Test
-    fun getMaxHealth() {
+    fun `getMaxHealth plane enemy`() {
         val width = 100f; val height = 100f
-        var coeff = 6f
-        var enemy = PlaneEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height), coeff)
-        var health = 200f * coeff
+        val coeff = 6f
+        val enemy = PlaneEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height), coeff)
+        val health = 200f * coeff
         assertEquals(health , enemy.getMaxHealth())
     }
 
     @Test
-    fun getHeight() {
-        val width = 100f
-        val height = 100f
-        var enemy = JetEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
-        val laser= MultipleBulletFactory().generate(1f, 1f, PlayerCollisionStrategy(), Bullet.Direction.DOWN, DisplayDimension(width, height))
-
+    fun `getMaxHealth jet enemy`() {
+        val width = 100f; val height = 100f
+        val enemy = JetEnemy(LaserBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
+        val health = 300f
+        assertEquals(health , enemy.getMaxHealth())
     }
 
     @Test
     fun shouldRemove() {
         val width = 100f; val height = 100f
-        var enemy = SpaceShipEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
+        val enemy = SpaceShipEnemy(GustBulletFactory(), Movement(1,2,3),  DisplayDimension(width, height))
         enemy.health = 0f
 
         assertTrue(enemy.shouldRemove())
     }
-
-
-//    @Test
-//    fun `exit screeen`(){
-//
-//        val width = 100f; val height = 100f
-//        var enemy = SpaceShipEnemy(GustBulletFactory(), Movement(4,3,600),  DisplayDimension(width, height))
-//
-//        while (!enemy.shouldRemove())
-//            enemy.update()
-//
-//        assertTrue(enemy.shouldRemove())
-//    }
 
 }
