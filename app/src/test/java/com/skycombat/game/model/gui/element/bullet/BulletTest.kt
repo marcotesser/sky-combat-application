60a3bf15ed8cffe341a6470684bdb453f2ab07ab
ApocalypseTH <36929115+ApocalypseTH@users.noreply.github.com>
2021-03-27 16:48:45 +0100

    Update BulletTest.kt

--- app/src/test/java/com/skycombat/game/model/gui/element/bullet/BulletTest.kt
+++ app/src/test/java/com/skycombat/game/model/gui/element/bullet/BulletTest.kt
@@ -20,7 +20,7 @@ class BulletTest {
     fun `shouldRemove gust bullet`() {
         val width = 100f
         val height = 100f
-        val bullet = GustBullet(2f, 2f, PlayerCollisionStrategy(), Bullet.Direction.UP, DisplayDimension(10f, 10f))
+        val bullet = GustBullet(2f, 2f, PlayerCollisionStrategy(), Bullet.Direction.UP, DisplayDimension(width, height))
         val enemy = PlaneEnemy(GustBulletFactory(), Movement(1, 2, 3), DisplayDimension(width, height))
         bullet.applyCollisionEffects(enemy)
         assertTrue(bullet.shouldRemove())
@@ -29,38 +29,82 @@ class BulletTest {
     fun `check life damage laser bullet`() {
         val width = 100f
         val height = 100f
-        val bullet = LaserBullet(2f, 2f, PlayerCollisionStrategy(), Bullet.Direction.UP, DisplayDimension(10f, 10f))
+        val bullet = LaserBullet(2f, 2f, PlayerCollisionStrategy(), Bullet.Direction.UP, DisplayDimension(width, height))
         val enemy = PlaneEnemy(GustBulletFactory(), Movement(1, 2, 3), DisplayDimension(width, height))
         bullet.applyCollisionEffects(enemy)
         assertTrue(enemy.health<enemy.getMaxHealth())
     }
 
     @Test
-    fun `getSpeed multiple bullet`() {
-        val width = 10f
-        val height = 100f
-        val bullet = MultipleBullet(2f, 0f, EnemyCollisionStrategy(), Bullet.Direction.DOWN, DisplayDimension(10f, 10f))
-        var enemy = PlaneEnemy(GustBulletFactory(), Movement(1, 2, 3), DisplayDimension(width, height))
-        val player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(10f, 100f))
-        player.setPosition(5f,100f)
-        player.health = 1f
-        for (i in 1..2)bullet.update()
-        bullet.applyCollisionEffects(player)
-        assertTrue(bullet.shouldRemove())
-    }
-
-    @Test
     fun `getSpeed classic bullet`() {
         val width = 10f
         val height = 100f
-        val bullet = ClassicBullet(2f, 0f, PlayerCollisionStrategy(), Bullet.Direction.UP, DisplayDimension(10f, 10f))
-        val enemy = JetEnemy(GustBulletFactory(), Movement(1, 2, 3), DisplayDimension(width, height))
-        val player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(10f, 100f))
+        val bullet = ClassicBullet(5f, 0f, EnemyCollisionStrategy(), Bullet.Direction.DOWN, DisplayDimension(width, height))
+        val player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(width, height))
+
         player.setPosition(5f,100f)
         player.health = 1f
+
         for (i in 1..2)bullet.update()
-        bullet.applyCollisionEffects(enemy)
-        assertTrue(bullet.shouldRemove())
+
+        if(player.collide(bullet))
+            bullet.applyCollisionEffects(player)
+
+        assertTrue(bullet.shouldRemove() && player.shouldRemove())
     }
+
+    @Test
+    fun `getSpeed gust bullet`() {
+        val width = 10f
+        val height = 100f
+        val bullet = GustBullet(5f, 0f, EnemyCollisionStrategy(), Bullet.Direction.DOWN, DisplayDimension(width, height))
+        val player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(width, height))
+
+        player.setPosition(5f,100f)
+        player.health = 1f
+
+        for (i in 1..2)bullet.update()
+
+        if(player.collide(bullet))
+            bullet.applyCollisionEffects(player)
+
+        assertTrue(bullet.shouldRemove() && player.shouldRemove())
+    }
+
+//    @Test
+//    fun `getSpeed laser bullet`() {
+//        val width = 10f
+//        val height = 270f
+//        val bullet = LaserBullet(5f, 0f, EnemyCollisionStrategy(), Bullet.Direction.DOWN, DisplayDimension(width, height))
+//        val player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(width, height))
+//
+//        player.setPosition(5f,height)
+//        player.health = 1f
+//
+//        bullet.update()
+//
+//        if(player.collide(bullet))
+//            bullet.applyCollisionEffects(player)
+//
+//        assertTrue(bullet.shouldRemove() && player.shouldRemove())
+//    }
+//
+//    @Test
+//    fun `getSpeed multiple bullet`() {
+//        val width = 10f
+//        val height = 119f
+//        val bullet = MultipleBullet(5f, 0f, EnemyCollisionStrategy(), Bullet.Direction.DOWN, DisplayDimension(width, height))
+//        val player = Player(10f, LinearAimedPositionMovement(), DisplayDimension(width, height))
+//
+//        player.setPosition(5f,height)
+//        player.health = 1f
+//
+//        for (i in 1..2)bullet.update()
+//
+//        if(bullet.collide(player))
+//            bullet.applyCollisionEffects(player)
+//
+//        assertTrue(bullet.shouldRemove() && player.shouldRemove())
+//    }
 
 }
